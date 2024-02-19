3e705b6a0494bdcbec0be6749c99df8d633d3a51
Kay <80385994+twilightyear@users.noreply.github.com>
2024-02-20 05:15:48 +0900

    Create A+B.cc

---
+++ 백준/Bronze/1000. A+B/A+B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+	int a,b;
+
+	std::cin >> a;
+	if (a<0){
+		std::cout << "조건 : a > 0"<< std::endl;
+		exit(0);
+	}
+	std::cin >> b;
+	if (b>=10){
+		std::cout << "조건 : b < 0"<< std::endl;
+		exit(0);
+	}
+	std::cout << a+b << std::endl;
+
+	return 0;
+}
