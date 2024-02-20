dea92dbdd8cfa67cf0a197c9b96f733684d42553
Kay <80385994+twilightyear@users.noreply.github.com>
2024-02-21 01:03:52 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+int main(){
+
+	double a,b, total=0;
+
+	std::cin >> a;
+	if (a<0){
+		std::cout << "조건 : a > 0"<< std::endl;
+		exit(0);
+	}
+	std::cin >> b;
+	if (b>=10){
+		std::cout << "조건 : b < 0"<< std::endl;
+		exit(0);
+	}
+
+	total=a/b;
+	std::cout << std::setprecision(10) << total << std::endl;
+
+	return 0;
+}
