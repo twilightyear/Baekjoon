02476e3408baea8733247df2a01571296f6cae9c
Kay <80385994+twilightyear@users.noreply.github.com>
2024-02-21 01:16:40 +0900

    Update A／B.cc
    오류수정

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -6,13 +6,13 @@ int main(){
 	double a,b, total=0;
 
 	std::cin >> a;
-	if (a<0){
+	if (a<=0){
 		std::cout << "조건 : a > 0"<< std::endl;
 		exit(0);
 	}
 	std::cin >> b;
 	if (b>=10){
-		std::cout << "조건 : b < 0"<< std::endl;
+		std::cout << "조건 : b < 10"<< std::endl;
 		exit(0);
 	}
 
