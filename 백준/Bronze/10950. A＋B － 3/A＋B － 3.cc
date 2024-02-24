0ba4195980ebbeeb5d20a71154e86492d0f8e9bd
Kay <80385994+twilightyear@users.noreply.github.com>
2024-02-25 03:07:05 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int c,i,j;
+	cin >> c;
+
+	for (int a = 1; a<=c;a++){
+		i = 0;
+		j = 0;
+		cin >> i >> j;
+
+		cout << i+j << endl;
+	}
+
+
+	return 0;
+} 
