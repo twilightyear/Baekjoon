e4603ec069808a0fe54330f523913a0f4b3f229b
Kay <80385994+twilightyear@users.noreply.github.com>
2024-02-26 20:08:37 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.cc
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -5,13 +5,13 @@ int main(){
 	int a,b;
 
 	std::cin >> a;
-	if (a<=0){
+	if (a<0){
 		std::cout << "조건 : a > 0"<< std::endl;
 		exit(0);
 	}
 	std::cin >> b;
 	if (b>=10){
-		std::cout << "조건 : b < 10"<< std::endl;
+		std::cout << "조건 : b < 0"<< std::endl;
 		exit(0);
 	}
 	std::cout << a*b << std::endl;
