27961c2e9915165e40bd31aa3f9750e2bde49d67
Kay <80385994+twilightyear@users.noreply.github.com>
2024-03-14 15:16:20 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+
+	while(1){
+		cin >> a >> b;
+		if(cin.eof()){
+			break;
+		} else {
+			cout << a+b << endl;
+		}
+
+	}
+	return 0;
+}
