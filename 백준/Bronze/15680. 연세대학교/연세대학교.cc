a27e7b5f0840e74635a93d593e73eb2134b8f5a3
Kay <80385994+twilightyear@users.noreply.github.com>
2024-03-28 17:12:53 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+
+	cin >> a;
+
+	if (a==1){
+		cout << "Leading the Way to the Future" << endl;
+	} else if (a==0){
+		cout << "YONSEI" << endl;
+	}
+	return 0;
+}
