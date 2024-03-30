87b83419ee2056fffa092b6d302978c4781001f1
Kay <80385994+twilightyear@users.noreply.github.com>
2024-03-31 01:42:40 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string username;
+
+	cin >> username;
+
+	cout << ":fan::fan::fan:" << "\n" << ":fan::" << username << "::fan:" << "\n" << ":fan::fan::fan:" << endl;
+	return 0;
+}
