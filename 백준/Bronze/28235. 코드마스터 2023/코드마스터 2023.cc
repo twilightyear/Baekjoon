b2f1b6b6bc6bdf44559600312502700c4a3f9407
Kay <80385994+twilightyear@users.noreply.github.com>
2024-04-16 09:20:35 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string word;
+	cin >> word;
+
+	if (word == "SONGDO"){
+		cout << "HIGHSCHOOL" << endl;
+	} else if (word == "CODE"){
+		cout << "MASTER" << endl;
+	} else if (word == "2023"){
+		cout << "0611" << endl;
+	} else if (word == "ALGORITHM"){
+		cout << "CONTEST" << endl;
+	} else 
+	return 0;
+}
