4acc1cf96b93b8bfd4d22c14b7905026e99abaa7
Kay <80385994+twilightyear@users.noreply.github.com>
2024-04-26 18:55:32 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+
+	if (n==1){
+		cout << "12 1600" << endl;
+	} else if (n==2){
+		cout << "11 894" << endl;
+	} else if (n==3){
+		cout << "11 1327" << endl;		
+	} else if (n==4){
+		cout << "10 1311" << endl;		
+	} else if (n==5){
+		cout << "9 1004" << endl;		
+	} else if (n==6){
+		cout << "9 1178" << endl;		
+	} else if (n==7){
+		cout << "9 1357" << endl;		
+	} else if (n==8){
+		cout << "8 837" << endl;		
+	} else if (n==9){
+		cout << "7 1055" << endl;		
+	} else if (n==10){
+		cout << "6 556" << endl;		
+	} else if (n==11){
+		cout << "6 773" << endl;
+	} 
+	return 0;
+}
