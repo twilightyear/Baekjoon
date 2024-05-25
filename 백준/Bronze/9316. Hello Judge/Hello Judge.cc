67584ec6733131b34eb81ebc496b67bfb1250a58
Kay <80385994+twilightyear@users.noreply.github.com>
2024-05-25 13:50:00 +0900

    [Bronze IV] Title: Hello Judge, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int cnt;
+	cin >> cnt;
+	for (int i=0;i<cnt;i++){
+		cout << "Hello World, Judge " << i+1 << "!"<< endl;
+	}
+	return 0;
+}
