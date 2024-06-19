595d35e4f24fb77711749ca2680747add52dba64
Kay <80385994+twilightyear@users.noreply.github.com>
2024-06-19 20:00:41 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int a,b;
+	while(1){
+		cin >> a >> b;
+		if (a==b and a==0){
+			break;
+		} else {
+			cout << a+b << endl;
+		}
+	}
+
+}
