97da63966e44133e8bdfe2c5c8a1f3698fb1ce79
Kay <80385994+twilightyear@users.noreply.github.com>
2024-06-28 19:56:20 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+
+	n--;
+
+	for (int i=0;i<n;i++){
+		for (int j=0;j<=i;j++){
+			cout << '*';
+		}
+		cout << '\n';
+	}
+
+	for (int i=n;i>=0;i--){
+		for (int j=i;j>=0;j--){
+			cout << '*';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
