6a12938af7d0d6762db2689ea182ace9e4bf4b78
Kay <80385994+twilightyear@users.noreply.github.com>
2024-06-29 16:23:17 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+
+	n--;
+
+	for (int k=n;k>=0;k--){
+		for (int i=n-k;i>0;i--){
+			cout << ' ';
+		}
+		for (int j=0;j<2*k+1;j++){
+			cout << '*';		
+		}
+		cout << '\n';
+	}
+
+	n++;
+	
+	for (int k=1;k<n;k++){
+		for (int i=n-k-1;i>0;i--){
+			cout << ' ';
+		}
+		for (int j=0;j<2*k+1;j++){
+			cout << '*';		
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
