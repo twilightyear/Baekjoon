075d96cf2795f4fc1426f7c3386e198cae41d192
Kay <80385994+twilightyear@users.noreply.github.com>
2024-08-05 16:03:46 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int N,x,y,cnt;
+	vector<pair<int,int>> array;
+	vector<int> result;
+	cin >> N;
+
+
+	for(int i=0;i<N;i++){
+		cin >> x >> y;
+		array.push_back({x,y});
+	}
+
+
+	for(int j=0;j<N;j++){
+		cnt=1;
+		for(int k=0;k<N;k++){
+			if(array[j].first < array[k].first and array[j].second < array[k].second){
+				cnt++;
+			}
+		}
+		result.push_back(cnt);
+	}
+
+
+	for(int l=0;l<N;l++){
+		if(l!=N-1){
+			cout << result[l] << ' ';
+		} else {
+			cout << result[l] << '\n';
+		}
+		
+	}
+
+
+	return 0;
+}
