fe13659193e50d4be9a9f3a70f40b85551dbb894
Kay <80385994+twilightyear@users.noreply.github.com>
2024-08-09 20:00:34 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int L,P,a,b,c,d,e;
+	cin >> L >> P >> a >> b >> c >> d >> e;
+
+
+	cout << a-L*P << ' ' << b-L*P << ' ' << c-L*P << ' ' << d-L*P << ' ' << e-L*P << '\n';
+	return 0;
+}
