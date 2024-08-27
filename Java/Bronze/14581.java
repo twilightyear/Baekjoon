9295c6fdfb03ea578215d3196e282d0f939de7fa
Kay <80385994+twilightyear@users.noreply.github.com>
2024-08-27 16:09:32 +0900

    Create 14581.java

---
+++ Java/Bronze/14581.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String name = scanner.nextLine();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + name + "::fan:");
+		System.out.println(":fan::fan::fan:");
+		
+		
+
+	}
+
+}
