989dc54a91580cb3579d492d14df745bbbc23d07
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-02 15:08:29 +0900

    Create 15680.java

---
+++ Java/Bronze/V/15680.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		if(n==0) {
+			System.out.println("YONSEI");
+		} else if(n==1) {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+
+}
