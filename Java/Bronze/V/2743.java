5de19d8cf7b534bfe3edf07d03755c675f23710f
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-14 19:13:01 +0900

    Create 2743.java

---
+++ Java/Bronze/V/2743.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String n = scanner.nextLine();
+		
+		System.out.println(n.length());
+		
+		
+
+	}
+
+}
