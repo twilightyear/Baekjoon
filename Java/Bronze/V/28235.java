69ae6987c216724c1903bdac99b384c9012fdb5f
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-15 15:55:24 +0900

    Create 28235.java

---
+++ Java/Bronze/V/28235.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String word = scanner.nextLine();
+		
+		if(word.equals("SONGDO")) {
+			System.out.println("HIGHSCHOOL");
+		} else if(word.equals("CODE")) {
+			System.out.println("MASTER");
+		} else if(word.equals("2023")) {
+			System.out.println("0611");
+		} else if(word.equals("ALGORITHM")) {
+			System.out.println("CONTEST");
+		}
+
+	}
+
+}
