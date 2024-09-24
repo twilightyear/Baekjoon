abbb0ca2ac860bddf6df0efb5871af6c16b45fa1
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-24 15:18:09 +0900

    Create 24078.java

---
+++ Java/Bronze/V/24078.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int X = scanner.nextInt();
+		
+		System.out.println(X%21);
+	}
+
+}
