f7e4ab8422e2c7eb7a72bcaeb95a133cdf8d9d89
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-26 15:05:47 +0900

    Create 29751.java

---
+++ Java/Bronze/V/29751.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		double W = scanner.nextInt();
+		double H = scanner.nextInt();
+		
+		System.out.println(W*H/2);
+
+	}
+
+}
