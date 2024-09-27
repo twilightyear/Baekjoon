d2e2f87274f2797916988517b1901dc3dbb4d710
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-27 09:02:19 +0900

    Create 32326.java

---
+++ Java/Bronze/V/32326.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		
+		System.out.println(a*3+b*4+c*5);
+		
+		
+
+	}
+
+}
