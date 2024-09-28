52f244284a7345aab6ebce88383ec856866fcf9d
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-28 21:33:09 +0900

    Create 8370.java

---
+++ Java/Bronze/V/8370.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n1 = scanner.nextInt();
+		int k1 = scanner.nextInt();
+		int n2 = scanner.nextInt();
+		int k2 = scanner.nextInt();
+		
+		System.out.println(n1*k1+n2*k2);
+
+	}
+
+}
