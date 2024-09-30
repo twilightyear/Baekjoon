640535ec66294a8cba1aeeb89d207e5d16167219
Kay <80385994+twilightyear@users.noreply.github.com>
2024-09-30 15:46:40 +0900

    Create 27331.java

---
+++ Java/Bronze/V/27331.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String A = scanner.nextLine();
+		String B = scanner.nextLine();
+		
+		System.out.println(A+B);
+		
+
+	}
+
+}
