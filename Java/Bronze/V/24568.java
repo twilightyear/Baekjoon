540b52accc61ad955511ed803247b5dbc1af498e
Kay <80385994+twilightyear@users.noreply.github.com>
2024-10-01 20:18:21 +0900

    Create 24568.java

---
+++ Java/Bronze/V/24568.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int R = scanner.nextInt();
+		int S = scanner.nextInt();
+		
+		System.out.println(R*8 + S*3-28);
+
+	}
+
+}
