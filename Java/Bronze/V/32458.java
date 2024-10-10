3a413997307edb74cc3f29064bbba81edbe80785
Kay <80385994+twilightyear@users.noreply.github.com>
2024-10-10 15:14:52 +0900

    Create 32458.java

---
+++ Java/Bronze/V/32458.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double n = scanner.nextDouble();
+		int m = (int)Math.floor(n);
+		System.out.println(m);
+
+	}
+
+}
