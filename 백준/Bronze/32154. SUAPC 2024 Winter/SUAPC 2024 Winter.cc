b2b7dac1c12d62ce5800f982b28580b706b2563a
Kay <80385994+twilightyear@users.noreply.github.com>
2024-10-12 16:07:31 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+
+	if(n==1){
+		cout << 11 << '\n';
+		cout << "A B C D E F G H J L M" << '\n';
+	} else if(n==2){
+		cout << 9 << '\n';
+		cout << "A C E F G H I L M" << '\n';
+	} else if(n==3){
+		cout << 9 << '\n';
+		cout << "A C E F G H I L M" << '\n';
+	} else if(n==4){
+		cout << 9 << '\n';
+		cout << "A B C E F G H L M" << '\n';
+	} else if(n==5){
+		cout << 8 << '\n';
+		cout << "A C E F G H L M" << '\n';
+	} else if(n==6){
+		cout << 8 << '\n';
+		cout << "A C E F G H L M" << '\n';
+	} else if(n==7){
+		cout << 8 << '\n';
+		cout << "A C E F G H L M" << '\n';
+	} else if(n==8){
+		cout << 8 << '\n';
+		cout << "A C E F G H L M" << '\n';
+	} else if(n==9){
+		cout << 8 << '\n';
+		cout << "A C E F G H L M" << '\n';
+	} else if(n==10){
+		cout << 8 << '\n';
+		cout << "A B C F G H L M" << '\n';
+	}
+
+	return 0;
+}
