72b69cf63f50aba716b0084615354bd2029296be
Kay <80385994+twilightyear@users.noreply.github.com>
2024-10-14 15:04:16 +0900

    Create 8545.java

---
+++ Java/Bronze/V/8545.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String word;
+		word = scanner.nextLine();
+		
+		System.out.print(word.charAt(2));
+		System.out.print(word.charAt(1));
+		System.out.print(word.charAt(0));
+		
+		
+	}
+
+}
