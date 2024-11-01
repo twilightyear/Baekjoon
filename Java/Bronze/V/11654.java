d9259d93a6984177702a867298396e8369593a31
Kay <80385994+twilightyear@users.noreply.github.com>
2024-11-01 09:22:12 +0900

    Create 11654.java

---
+++ Java/Bronze/V/11654.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.nextLine();
+		char c = s.charAt(0);
+		
+		System.out.println(c-0);
+		
+
+	}
+
+}
