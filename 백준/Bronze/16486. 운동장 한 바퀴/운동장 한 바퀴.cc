ad9f38f4f0f6ff5941f9e328a50f981770efb5fb
Kay <80385994+twilightyear@users.noreply.github.com>
2024-11-07 18:13:15 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-    int d1, d2;
-    float pi = 3.141592;
-    cin>>d1>>d2;
-    cout << fixed;
-    cout.precision(6);
-    cout<<d1*2 + 2*pi*d2;
-    return 0;
+	int d1, d2;
+	float pi = 3.141592;
+	cin >> d1 >> d2;
+
+	cout.precision(6);
+	cout << fixed << (d1*2) + (2*pi*d2) << '\n';
+
+	return 0;
 }
